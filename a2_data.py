bc7ee5ef83faf4ab0bcf444889e1b42006579ebf
Shreyash Umale <shreyashumale@outlook.com>
2025-10-22 13:01:29 +0530

    Update a1_mian.py with new text

--- a2_data.py
+++ a2_data.py
@@ -11,18 +11,32 @@ lesson_data = {
                 "Shubh Sandhyakal": "Good Evening",
                 "Shubh Ratri": "Good Night",
             },
-            "quiz1": [
-                {
-                    "question": "What does" "'" "Namaskar" "'" "mean?",
-                    "options": ["Hello", "Thanks", "GoodBye"],
-                    "answer": "Hello",
-                },
-                {
-                    "question": "Which phrase means" "'" "Good Morning" "'" "?",
-                    "options": ["Shubh Ratri", "Suprabhat", "Namaskar"],
-                    "answer": "Suprabhat",
-                },
-            ],
+            "quiz_section": {
+                "quiz1": [
+                    {
+                        "question": "What does" "'" "Namaskar" "'" "mean?",
+                        "options": ["Hello", "Thanks", "GoodBye"],
+                        "answer": "Hello",
+                    },
+                    {
+                        "question": "Which phrase means" "'" "Suprabhat" "'" "?",
+                        "options": ["Good Morning", "Good Night", "Good Evening"],
+                        "answer": "Good Morning",
+                    },
+                ],
+                "quiz2": [
+                    {
+                        "question": "What does" "'" "Good Night" "'" "mean?",
+                        "options": ["Shubh Ratri", "Namaskar", "Suprabhat"],
+                        "answer": "Shubh Ratri",
+                    },
+                    {
+                        "question": "Which phrase means" "'" "Good Evening" "'" "?",
+                        "options": ["Shubh Sandhyakal", "Suprabhat", "Namaskar"],
+                        "answer": "Shubh Sandhyakal",
+                    },
+                ],
+            }, 
         },
         {
             "title": "Basic Polite Phrases",
@@ -31,30 +45,32 @@ lesson_data = {
                 "Krupa": "Please",
                 "Maaf Kara": "Sorry",
             },
-            "quiz1": [
-                {
-                    "question": "What does" "'" "Dhanyavād" "'" "mean?",
-                    "options": ["Thanks", "Namaskar", "Shubh Ratri"],
-                    "answer": "Thanks",
-                },
-                {
-                    "question": "Which word means" "'" "Please" "'" "?",
-                    "options": ["Krupa", "Namaskar", "Shubh Ratri"],
-                    "answer": "Krupa",
-                },
-            ],
-            "quiz2": [
-                {
-                    "question": "What does" "'" "Sorry" "'" "mean?",
-                    "options": ["Maaf Kara", "Dhanyawad", "Krupa"],
-                    "answer": "Maaf Kara",
-                },
-                {
-                    "question": "Which word means" "'" "Thank You" "'" "?",
-                    "options": ["Dhanyawad", "Krupa", "Maaf Kara"],
-                    "answer": "Dhanyawad",
-                },
-            ],
+            "quiz_section": {
+                "quiz1": [
+                    {
+                        "question": "What does" "'" "Dhanyavād" "'" "mean?",
+                        "options": ["Thanks", "Namaskar", "Shubh Ratri"],
+                        "answer": "Thanks",
+                    },
+                    {
+                        "question": "Which word means" "'" "Please" "'" "?",
+                        "options": ["Krupa", "Namaskar", "Shubh Ratri"],
+                        "answer": "Krupa",
+                    },
+                ],
+                "quiz2": [
+                    {
+                        "question": "What does" "'" "Sorry" "'" "mean?",
+                        "options": ["Maaf Kara", "Dhanyawad", "Krupa"],
+                        "answer": "Maaf Kara",
+                    },
+                    {
+                        "question": "Which word means" "'" "Thank You" "'" "?",
+                        "options": ["Dhanyawad", "Krupa", "Maaf Kara"],
+                        "answer": "Dhanyawad",
+                    },
+               ],
+            },
         },
         {
             "title": "Numbers 1-5", 
@@ -65,30 +81,32 @@ lesson_data = {
                 "4": "Char",
                 "5": "Panch",
             },
-            "quiz1": [
-                {
-                    "question": "What is" "'" "3" "'" "in Marathi?",
-                    "options": ["Teen", "Don", "Char"],
-                    "answer": "Teen",
-                },
-                {
-                    "question": "Which number is" "'" "Panch" "'" "?",
-                    "options": ["4", "5", "2"],
-                    "answer": "5",
-                },
-            ],
-            "quiz2": [
-                {
-                    "question": "What is" "'" "2" "'" "in Marathi?",
-                    "options": ["Don", "Ek", "Char"],
-                    "answer": "Don",
-                },
-                {
-                    "question": "Which number is" "'" "Char" "'" "?",
-                    "options": ["3", "4", "5"],
-                    "answer": "4",
-                },
-            ]
+            "quiz_section": {
+                "quiz1": [
+                    {
+                        "question": "What is" "'" "3" "'" "in Marathi?",
+                        "options": ["Teen", "Don", "Char"],
+                        "answer": "Teen",
+                   },
+                    {
+                        "question": "Which number is" "'" "Panch" "'" "?",
+                        "options": ["4", "5", "2"],
+                        "answer": "5",
+                    },
+                ],
+                "quiz2": [
+                    {
+                        "question": "What is" "'" "2" "'" "in Marathi?",
+                        "options": ["Don", "Ek", "Char"],
+                        "answer": "Don",
+                    },
+                    {
+                        "question": "Which number is" "'" "Char" "'" "?",
+                        "options": ["3", "4", "5"],
+                        "answer": "4",
+                    },
+               ],
+            },
         },
         {
             "title": "Days of the week",
@@ -101,30 +119,32 @@ lesson_data = {
                 "Saturday": "Shanivar",
                 "Sunday": "Ravivar",
             },
-            "quiz1": [
-                {
-                    "question": "What is" "'" "Mangalvar" "'" "?",
-                    "options": ["Tuesday", "Monday", "Friday"],
-                    "answer": "Tuesday",
-                },
-                {
-                    "question": "Which day is" "'" "Shukravar" "'" "?",
-                    "options": ["Friday", "Sunday", "Monday"],
-                    "answer": "Friday",
-                },
-            ],
-            "quiz2": [
-                {
-                    "question": "What is" "'" "Saturday" "'" "in Marathi?",
-                    "options": ["Shanivar", "Ravivar", "Somvar"],
-                    "answer": "Shanivar",
-                },
-                {
-                    "question": "Which day is" "'" "Wednesday" "'" "?",
-                    "options": ["Budhvar", "Guruvar", "Mangalvar"],
-                    "answer": "Budhvar",
-                },
-            ],
+            "quiz_section": {
+                "quiz1": [
+                    {
+                        "question": "What is" "'" "Mangalvar" "'" "?",
+                        "options": ["Tuesday", "Monday", "Friday"],
+                        "answer": "Tuesday",
+                    },
+                    {
+                        "question": "Which day is" "'" "Shukravar" "'" "?",
+                        "options": ["Friday", "Sunday", "Monday"],
+                        "answer": "Friday",
+                    },
+                ],
+                "quiz2": [
+                    {
+                        "question": "What is" "'" "Saturday" "'" "in Marathi?",
+                        "options": ["Shanivar", "Ravivar", "Somvar"],
+                        "answer": "Shanivar",
+                    },
+                    {
+                        "question": "Which day is" "'" "Wednesday" "'" "?",
+                        "options": ["Budhvar", "Guruvar", "Mangalvar"],
+                        "answer": "Budhvar",
+                    },
+                ],
+            }
         },
         {
             "title": "Colors",
@@ -135,30 +155,32 @@ lesson_data = {
                 "Yellow": "Pivla",
                 "Black": "Kala",
             },
-            "quiz1": [
-                {
-                    "question": "What color is" "'" "Neela" "'" "?",
-                    "options": ["Blue", "Red", "Back"],
-                    "answer": "Blue",
-                },
-                {
-                    "question": "Which word means" "'" "Yellow" "'" "?",
-                    "options": ["Pivla", "Laal", "Kaala"],
-                    "answer": "Pivla",
-                },
-            ],
-            "quiz2": [
-                {
-                    "question": "What color is" "'" "Black" "'" "?",
-                    "options": ["Kala", "Hirva", "Laal"],
-                    "answer": "Kala",
-                },
-                {
-                    "question": "Which word means" "'" "Green" "'" "?",
-                    "options": ["Hirva", "Nila", "Pivla"],
-                    "answer": "Hirva",
-                }
-            ]
+            "quiz_section": {
+                "quiz1": [
+                    {
+                        "question": "What is" "'" "Laal" "'" "in English?",
+                        "options": ["Red", "Blue", "Green"],
+                        "answer": "Red",
+                    },
+                    {
+                        "question": "Which color is" "'" "Pivla" "'" "?",
+                        "options": ["Yellow", "Black", "Green"],
+                        "answer": "Yellow",
+                    },
+                ],
+                "quiz2": [
+                    {
+                        "question": "What is" "'" "Black" "'" "in Marathi?",
+                        "options": ["Kala", "Nila", "Hirva"],
+                        "answer": "Kala",
+                    },
+                    {
+                        "question": "Which color is" "'" "Green" "'" "?",
+                        "options": ["Hirva", "Laal", "Pivla"],
+                        "answer": "Hirva",
+                    },
+                ],
+            },
         },
     ],
 }
